91470cf10e72169b881c9de464435641e5ff0a6a
Eduardo Freitas <edufreitas.pb@gmail.com>
2020-09-18 16:15:44 -0300

    FINAL

--- metodo_bissecao.m
+++ metodo_bissecao.m
@@ -8,24 +8,26 @@
 clear all, close all, clc, 
 a=0; 
 b=2; 
-Nmax=10; 
-tol=1e-3; 
+Nmax=100; 
+tol=1e-42; 
 n=1; 
 e(n) = b-a;
 xi(n)=0;
-ya = a^3-2*a^2+3*a-1; 
-yb = b^3-2*b^2+3*b-1;
+
+# EQUAÇÃO ALGÉBRICA 
+  %ya = a^3-2*a^2+3*a-1; 
+  %yb = b^3-2*b^2+3*b-1;
+
+# EQUAÇÃO TRANSCENDENTE
+  ya = a*exp(a) - 42;
+  yb = b*exp(b) - 42;
 
 while(e(n)>tol && n<=Nmax)
   n=n+1;  
   xi(n)= a+(b-a)/2;
-  yi(n)= xi(n)^3-2*xi(n)^2+3*xi(n)-1;
+  yi(n)= xi(n)*exp(xi(n))-42; % MUDAR EQUAÇÃO AQUI
   if (yi(n)==0), break; end
-  if (ya*yi(n)<0),
-    b=xi(n);
-  else,
-    a=xi(n);
-  end
+  if (ya*yi(n)<0), b=xi(n); else, a=xi(n); end
   e(n)=abs(xi(n)-xi(n-1)); 
 end
 
