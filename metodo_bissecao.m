0c3255a575725bdf3bb98d5ca5a4383e2dfbba51
Eduardo Freitas <edufreitas.pb@gmail.com>
2020-09-19 15:17:54 -0300

    Update metodo_bissecao.m

--- metodo_bissecao.m
+++ metodo_bissecao.m
@@ -5,28 +5,32 @@
 % EXERCICIO 1-A
 % AUTOR: ANTONIO BATISTA E EDUARDO FREITAS
 
-clear all, close all, clc,
-a=0;
+clear all, close all, clc, 
+a=0; 
 b=2; 
 Nmax=42; 
-tol=1e-12;
-n=1;
+tol=1e-3; 
+n=1; 
 e(n) = b-a;
 xi(n)=0;
-ya = a*exp(a)- ;
-yb = a*exp(a)- ;
+
+# EQUAÇÃO ALGÉBRICA 
+  %xi(n)^3 - 2*xi(n)^2 + 3*xi(n) -1:
+  %ya = a^3-2*a^2+3*a-1; 
+  %yb = b^3-2*b^2+3*b-1;
+
+# EQUAÇÃO TRANSCENDENTE
+  %xi(n)*exp(xi(n))-pi;
+  ya = a*exp(a) - pi;
+  yb = b*exp(b) - pi;
 
 while(e(n)>tol && n<=Nmax)
-	n=n+1;
-	xi(n)= a+(b-a)/2;
-	yi(n)= xi(n)*exp(xi(n))- ;
-	if (yi(n)==0), break; end
-	if (ya*yi(n)<0),
-b=xi(n);
-else,
-a=xi(n); 
-end
-e(n)=abs(xi(n)-xi(n-1));
+  n=n+1;  
+  xi(n)= a+(b-a)/2;
+  yi(n)= xi(n)*exp(xi(n))- pi; % MUDAR EQUAÇÃO AQUI
+  if (yi(n)==0), break; end
+  if (ya*yi(n)<0), b=xi(n); else, a=xi(n); end
+  e(n)=abs(xi(n)-xi(n-1)); 
 end
 
 N=0:n - 3;
