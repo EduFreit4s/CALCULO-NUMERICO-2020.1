5f6e7c602e9a62c78ff32d8a45592cd67c74334f
Eduardo Freitas <edufreitas.pb@gmail.com>
2020-09-25 14:12:28 -0300

    medoto_das_cordas

--- metodo_secante.m
+++ metodo_secante.m
@@ -8,34 +8,34 @@
 clc, clear all, close all, format short,
 
 TOL = 1E-4;     %EPSILON
-K=20;           %ITERAÇÕES
+K=20;           %ITERAÃ‡Ã•ES
 a = 0; b = 2;   %PONTOS
 e(1) = 0;
 
-#MINHA EQUAÇÃO TRANSCENDENTE:     x(n)*exp(x(n)) - pi;
+#MINHA EQUAÃ‡ÃƒO TRANSCENDENTE:     x(n)*exp(x(n)) - pi;
 
 #f(x) = x*e^x - pi 
 #f'(x) = e^x(1+x)
 #f''(x) = e^x(2+x) 
  
-#ESCOLHA UM PONTO ALEATÓRIO (A OU B) QUE ESSA CONDIÇÃO SEJA 
+#ESCOLHA UM PONTO ALEATÃ“RIO (A OU B) QUE ESSA CONDIÃ‡ÃƒO SEJA 
 #VERDADEIRA  [f(c)*f''(c) > 0] PARA DEFINIR O PONTO FIXO 'C'
 
  % Fazendo c = a, temos
 
  # f(2) = 2*exp(2) - pi = 14.77811
  # f''(2) = exp(2)*(2+2) = 29.55622
- # f(c)*f''(c) > 0 = 14.77811*29.55622 = 436.78513, ok
+ # f(c)*f''(c) > 0 = 14.77811*29.55622 = 436.78513 > 0, ok
  # logo c = 2, x(1) = 0
 
 c = b; 
 x(1)=a;
 
-#TUA EQUAÇÃO COM f(ponto fixo definido acima) 
-fc = c*exp(c) - pi;                             %TROCAR EQUAÇÃO AQUI
+#TUA EQUAÃ‡ÃƒO COM f(ponto fixo definido acima) 
+fc = c*exp(c) - pi;                             %TROCAR EQUAÃ‡ÃƒO AQUI
 
 for n=1:K
-  fxn = x(n)*exp(x(n)) - pi;                    %TROCAR EQUAÇÃO AQUI
+  fxn = x(n)*exp(x(n)) - pi;                    %TROCAR EQUAÃ‡ÃƒO AQUI
   f(n) = fxn;
   x(n+1) = x(n)-fxn*(x(n)-c)/(fxn-fc);
   e(n+1) = abs(x(n+1)-x(n));
@@ -44,5 +44,5 @@ for n=1:K
   endif
 endfor
 
-f(n+1)= x(n+1)*exp(x(n+1)) - pi;                %TROCAR EQUAÇÃO AQUI
+f(n+1)= x(n+1)*exp(x(n+1)) - pi;                %TROCAR EQUAÃ‡ÃƒO AQUI
 disp([(0:n)' x' f' e'])
